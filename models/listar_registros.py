e6f58bce852a92427e31c4e8c0174fb88699650e
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2023-05-22 21:29:48 -0300

    bug fix

--- models/listar_registros.py
+++ models/listar_registros.py
@@ -1,4 +1,8 @@
 def listar_registros(chave, dados):
     registros = dados[chave]  # Obtém a lista de registros da chave especificada nos dados
-    for registro in registros:  # Itera sobre cada registro na lista de registros
-        print(registro)  # Imprime o registro
+    
+    if not registros:  # Verifica se a lista de registros está vazia
+        print("\n\nNão há estudantes cadastrados\n\n")
+    else:
+        for registro in registros:  # Itera sobre cada registro na lista de registros
+            print(registro)  # Imprime o registro
